3c24e94cb3dde86a68f80116f359e19a2dc0e051
ThreadedDev <126824270+ThreadedDev@users.noreply.github.com>
2024-06-24 17:46:02 +0300

    Update and rename main.c to HelloWorldInC.c

---
+++ C/HelloWorldInC.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main() 
+{
+    char s[100];
+    fprintf( stdout, "Hello, World!\nWelcome to C programming." );   
+    return 0;
+}
